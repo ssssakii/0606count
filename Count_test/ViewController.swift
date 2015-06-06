818e87bc9a5c6fe5f715d8234272abe9c439bb5e
高田早記 <takadasaki@takadasaki-no-MacBook-Air.local>
2015-06-06 14:21:27 +0900

    Initial Commit

---
+++ Count_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count_test
+//
+//  Created by 高田早記 on 2015/06/06.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
