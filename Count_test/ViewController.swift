1b6c8f5e89ba3e46dab3b720eebc47b7a613b3c0
高田早記 <takadasaki@takadasaki-no-MacBook-Air.local>
2015-06-06 16:02:57 +0900

    first commit

--- Count_test/ViewController.swift
+++ Count_test/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController {//一番最初に出てくる画面
+    
+    var number: Int=0//変数宣言。かつ、初期化
+    @IBOutlet var label: UILabel!//Labelを宣言する。varは変数宣言。labelは名前。
 
-    override func viewDidLoad() {
+    override func viewDidLoad() {　//コントローラが一番最初に出てくる画面
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func plus(){　//＠IBAction func 名前:動作を指定する
+    number = number + 1
+    label.text=String(number)//labelというtext(文字)をnumberに入れる。int型をSpring型に変換。
+    
+    }
 
 }
 
